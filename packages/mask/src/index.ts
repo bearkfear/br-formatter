ef9a50308e39a4ac0f6a79bb80e382cbcb394875
bearkfear <enrico@moinhosul.com.br>
2022-11-17 21:09:19 +0000

    refactor(mask): melhora testes, corrige bugs e deixa mais legivel

--- packages/mask/src/index.ts
+++ packages/mask/src/index.ts
@@ -1,3 +1,6 @@
 export { stringify } from "./stringify";
 export { parse } from "./parse";
 export { masks } from "./masks";
+export { defaultTokens } from "./tokens";
+export type { Mask, Value } from "./masker";
+export type { Tokens } from "./tokens";
